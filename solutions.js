abe5ed700abd4cb0c9214feb27a0b0e0c74d8584
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-06-10 09:59:30 -0500

    101_exercise_review

--- solutions.js
+++ solutions.js
@@ -1,2 +1,20 @@
 // Write your Solutions here!!
+function doingJavaScriptRightNow() {
+    return true;
+}
 
+function onMarsRightNow() {
+    return false;
+}
+
+function sayHello(input){
+   if (typeof input === "string" && input !== "" && isNaN(input)){
+       return "Hello, " + input + "!";
+   } else {
+       return "Hello, world!"
+   }
+}
+
+function plusTwo() {
+    
+}
